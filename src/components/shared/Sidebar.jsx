ad913b2ba10b627d15469cb4e7575f7c5152e9b7
VeronnicamendezP <veronicamendezp@outlook.com>
2024-02-28 18:16:01 -0500

    I created menu states, responsive mobile menu and part of the header

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { RiHome4Line, RiWaterPercentLine, RiPieChart2Line, RiMailLine, RiNotification2Line, RiSettings5Line, RiDoorOpenLine } from "react-icons/ri";
+import { RiHome4Line, RiWaterPercentLine, RiShoppingCart2Line, RiMailLine, RiNotification2Line, RiSettings5Line, RiLogoutBoxRLine } from "react-icons/ri";
 
 
 
-export const Sidebar = () => {
+export const Sidebar = (props) => {
+  const { showMenu } = props; //destrucuturing
+
+
+
   return (
-    <div className='bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full'>
+    <div className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 transition-all ${showMenu ? 'left-0' : '-left-full'} `} > {/* flex, para el posicionar el log out */}
+    {/* si el estado showmenu es positivo ad ... si no .. */}
+
+      <div>
       <ul className='pl-4'>
         <li>
         <h1 className='text-2xl text-gray-300 uppercase font-bold text-center my-5'>Logo</h1>
@@ -26,7 +33,7 @@ export const Sidebar = () => {
 
         <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
           <a href='#' className=' group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors'>
-          < RiPieChart2Line className='text-xl '/>
+          < RiShoppingCart2Line className='text-xl '/>
           </a>
         </li>
 
@@ -48,13 +55,21 @@ export const Sidebar = () => {
           </a>
         </li>
 
-        <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
-          <a href='#' className=' group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors'>
-          < RiDoorOpenLine className='text-xl '/>
-          </a>
-        </li>
+
 
       </ul>
+      </div>
+
+      <div>
+        <ul className='pl-4'>
+        <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+          <a href='#' className=' group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors'>
+          < RiLogoutBoxRLine className='text-xl '/>
+          </a>
+        </li>
+        </ul>
+      </div>
+
     </div>
   )
 };
